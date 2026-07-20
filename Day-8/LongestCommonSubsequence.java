ff5f99ce0a4785227e83d19df71822c4c2f419af
Tushya Gupta <tushya2311.be23@chitkara.edu.in>
2026-07-20 10:09:39 +0530

    Day 8

---
+++ Day-8/LongestCommonSubsequence.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class LongestCommonSubsequence {
+    public static int lcs(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+        System.out.println("Longest Common Subsequence length: " + lcs(str1, str2));
+    }
+}
