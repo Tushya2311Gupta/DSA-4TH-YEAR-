6fedc4448435a5278cdb12caeb99edcc23cec258
Tushya Gupta <tushya2311.be23@chitkara.edu.in>
2026-07-24 14:35:57 +0530

    Day 12

--- Day-12/sandwich.java
+++ Day-12/sandwich.java
@@ -4,8 +4,6 @@ public class sandwich {
     public static int countStudents(int[] students, int[] sandwiches) {
     int circleCount = 0;
     int squareCount = 0;
-    
-    // Count how many students prefer each type of sandwich
     for (int student : students) {
         if (student == 0) {
             circleCount++;
@@ -13,48 +11,46 @@ public class sandwich {
             squareCount++;
         }
     }
-    
-    // Iterate through the sandwiches stack
     for (int sandwich : sandwiches) {
         if (sandwich == 0) {
             if (circleCount > 0) {
-                circleCount--; // A student eats the circular sandwich
+                circleCount--; 
             } else {
-                break; // No one left wants this circular sandwich, queue halts
+                break;
             }
         } else {
             if (squareCount > 0) {
-                squareCount--; // A student eats the square sandwich
+                squareCount--;
             } else {
-                break; // No one left wants this square sandwich, queue halts
+                break;
             }
         }
     }
     
-    // The number of students unable to eat
+
     return circleCount + squareCount;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
 
         System.out.print("Enter number of students: ");
-        int n = scanner.nextInt();
+        int n = sc.nextInt();
         int[] students = new int[n];
         System.out.println("Enter student preferences (0 for circle, 1 for square):");
         for (int i = 0; i < n; i++) {
-            students[i] = scanner.nextInt();
+            students[i] = sc.nextInt();
         }
 
         System.out.print("Enter number of sandwiches: ");
-        int m = scanner.nextInt();
+        int m = sc.nextInt();
         int[] sandwiches = new int[m];
         System.out.println("Enter sandwich stack order (0 for circle, 1 for square):");
         for (int i = 0; i < m; i++) {
-            sandwiches[i] = scanner.nextInt();
+            sandwiches[i] = sc.nextInt();
         }
 
-        scanner.close();
+        sc.close();
         int result = countStudents(students, sandwiches);
         System.out.println("Students unable to eat: " + result);
     }
--- Day-12/secret.java
+++ Day-12/secret.java
@@ -3,48 +3,41 @@ import java.util.Scanner;
 public class secret {
     public static int peopleAwareOfSecret(int n, int delay, int forget) {
         long mod = 1000000007;
-        // dp[i] will store the number of NEW people who discover the secret on day i.
         long[] dp = new long[n + 1];
-        dp[1] = 1; // On day 1, one person discovers the secret.
+        dp[1] = 1;
         
-        long activeSharers = 0; // People who can currently share the secret
+        long activeSharers = 0;
         
         for (int i = 2; i <= n; i++) {
-            // Add people who just started sharing today
             if (i - delay > 0) {
                 activeSharers = (activeSharers + dp[i - delay]) % mod;
             }
-            // Subtract people who forgot the secret today
             if (i - forget > 0) {
                 activeSharers = (activeSharers - dp[i - forget] + mod) % mod;
             }
-            
-            // The number of new people today equals the number of active sharers
             dp[i] = activeSharers;
         }
         
         long totalPeople = 0;
-        // Sum up all the people who learned the secret in the last 'forget' days
         for (int i = Math.max(1, n - forget + 1); i <= n; i++) {
             totalPeople = (totalPeople + dp[i]) % mod;
         }
-        
         return (int) totalPeople;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter n (total days): ");
-        int n = scanner.nextInt();
+        int n = sc.nextInt();
 
         System.out.print("Enter delay (days until a person can share): ");
-        int delay = scanner.nextInt();
+        int delay = sc.nextInt();
 
         System.out.print("Enter forget (days until a person forgets): ");
-        int forget = scanner.nextInt();
+        int forget = sc.nextInt();
 
-        scanner.close();
+        sc.close();
 
         int result = peopleAwareOfSecret(n, delay, forget);
         System.out.println("People aware of the secret after " + n + " days: " + result);
